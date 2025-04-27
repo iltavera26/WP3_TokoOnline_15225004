92d445f60a8793f88b9cd9d992a1746ee8d0c332
iltavera26 <iltaverapaula10@gmail.com>
2025-04-27 22:11:13 +0700

    first commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -17,15 +17,16 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
+
     protected $table = "user";
     protected $fillable = [
-    'nama',
-    'email',
-    'role',
-    'status',
-    'password',
-    'hp',
-    'foto',
+        'nama',
+        'email',
+        'role',
+        'status',
+        'password',
+        'hp',
+        'foto',
     ];
 
     /**
